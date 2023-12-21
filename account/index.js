e17dcef88d3bc240a6d7f70cd5c66bc9a2048cf8
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2023-12-21 14:24:30 +0300

    Added some features

--- account/index.js
+++ account/index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const router = express.Router() // Instantiate a new router
 
 const prisma = require('../lib/prisma.js')
+const tg_tools = require('../methods/tg_tools.js')
 const bank = require('../methods/bank.js')
 
 const limit = 15
+const err_codes = ['invalid_transaction', 'account_not_exist', 'account_blocked', 'insufficient_funds']
 
 router.get('/', async function (req, res) {
     const account = (await bank.get_accounts(req.session.user_id))[0]
     const codes = await bank.get_codes(account.id)
+
+    const error = req.query.error
 
     const page = req.query.page ? Number(req.query.page) : 1
     const transactions = await bank.get_transactions(account.id, (page - 1) * limit, limit)
@@ -20,6 +24,7 @@ router.get('/', async function (req, res) {
     return res.render('account/index.html', {
         base: 'base.html',
         title: 'Аккаунт',
+        error: error,
         id: account.id,
         balance: await bank.balance(account.id),
         created_at: account.created_at,
@@ -80,6 +85,30 @@ router.post('/telegram/delete', async (req, res) => {
     await prisma.telegram.delete({ where: { account_id: account.id } })
 
     return res.redirect('/account/telegram')
+})
+
+router.post('/transfer', async (req, res) => {
+    const account = (await bank.get_accounts(req.session.user_id))[0]
+
+    const to_id = Number(req.body.to_id)
+    const amount = Number(req.body.amount)
+    const description = req.body.description
+
+    if (!to_id || !amount || !description || to_id < 0 || amount <= 0)
+        return res.redirect('/account')
+
+    try {
+        const transaction = await bank.transfer(account.id, to_id, amount, description)
+
+        const telegram = await bank.get_telegram(transaction.to_id)
+        tg_tools.transaction_notification(telegram, transaction)
+    }
+    catch (e) {
+        if (err_codes.includes(e.code))
+            return res.redirect(`/account?error=${e.message}`)
+    }
+
+    return res.redirect('/account')
 })
 
 module.exports = router
